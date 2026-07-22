d69cbb0c9e33b708df122f69f684f25df193559e
vanshjuyal <vansh.juyal.cse.2025@miet.ac.in>
2026-07-22 10:34:49 +0530

    Create arraydeletion_22-july-2026.c

---
+++ arraydeletion_22-july-2026.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int arr[100] ,n;
+    printf("enter the sizeof array\n");
+    scanf("%d",&n);
+    printf("now enter elements:\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    printf("enter the pos to be deleted\n");
+    int pos;
+    scanf("%d",&pos);
+    for(int i=pos-1;i<n-1;i++){
+        arr[i]=arr[i+1];
+    }
+    for(int i=n;i>=pos;i--){
+        arr[i]=arr[i-1];
+    }
+    printf("elements of array are: \n");
+    for(int i=0;i<n-1;i++){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
