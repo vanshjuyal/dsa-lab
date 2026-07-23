926d4dc3bb4c3829d4a1b60fbb319a6e0777642f
vanshjuyal <vansh.juyal.cse.2025@miet.ac.in>
2026-07-23 06:21:39 -0400

    arraydeletion_22-july-2026.c

--- arraydeletion_22-july-2026.c
+++ arraydeletion_22-july-2026.c
@@ -1,21 +1,28 @@
-#include <stdio.h>
-int main() {
-    int arr[100] ,n;
-    printf("enter the sizeof array\n");
+#include<stdio.h>
+void main(){
+    int n;
+    printf("enter size of arrr\n");
     scanf("%d",&n);
-    printf("now enter elements:\n");
-    for(int i=0;i<n;i++){
+    int arr[n];
+    printf("enter elements\n");
+    for(int i=0;i<n;i++)
         scanf("%d",&arr[i]);
-    }
     printf("enter the pos to be deleted\n");
     int pos;
     scanf("%d",&pos);
-    for(int i=pos-1;i<n-1;i++){
-        arr[i]=arr[i+1];
+    if(pos<1 || pos>n){
+        printf("invalid pos\n");
+    } 
+    else{
+        for(int i=pos-1;i<n-1;i++){
+            arr[i]=arr[i+1];
+        }
+        n--;
+
     }
-    printf("elements of array are: \n");
-    for(int i=0;i<n-1;i++){
+    printf("elements of array are:\n");
+    for(int i=0;i<n;i++){
         printf("%d\n",arr[i]);
     }
-    return 0;
+
 }
